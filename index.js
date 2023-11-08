455310b5bfee48d41ff89c671796a2c351b4db3e
Dipto Shaha <diptoshaha2635@gmail.com>
2023-11-08 20:55:01 +0600

    Code Refresh

--- index.js
+++ index.js
@@ -164,8 +164,7 @@ async function run() {
     
   
   } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
+git    //await client.close();
   }
 }
 run().catch(console.dir);
